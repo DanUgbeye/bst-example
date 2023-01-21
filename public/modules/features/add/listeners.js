c46d97a6ce83ba6196fac601ba538c3460dbc852
Daniel Ugbeye <ugbeyellionz@gmail.com>
2023-01-21 19:51:14 +0100

    feat: implemented add item functionality

--- public/modules/features/add/listeners.js
+++ public/modules/features/add/listeners.js
@@ -1,7 +1,9 @@
 import addForm from "./data.js";
 import getInput from "../../utils/getInput.util.js";
 import onFormSubmit from "../../utils/onFormSubmit.util.js";
-import validateInput from "../../utils/validateInput.util.js"
+import validateInput from "../../utils/validateInput.util.js";
+import BST from "../../data/tree.data.js";
+import { successModal, errorModal } from "../modals/data.js";
 
 export default function initListeners() {
   getInput("add-item-input", (value) => {
@@ -16,6 +18,13 @@ export default function initListeners() {
       return;
     }
     //TODO add item to tree
-    console.log(addForm);
+    BST.insert(addForm.value.get());
+    successModal.message = "Item added successfully";
+    successModal.open.set(true);
+
+    // reset values
+    const inputElement = window.document.getElementById("add-item-input");
+    inputElement.value = "";
+    addForm.value.set("");
   });
 }
