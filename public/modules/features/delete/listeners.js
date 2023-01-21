d52934258a5c04d4956a2dffe73946c7a8e813ce
Daniel Ugbeye <ugbeyellionz@gmail.com>
2023-01-21 19:52:42 +0100

    feat: implemented delete functionality

--- public/modules/features/delete/listeners.js
+++ public/modules/features/delete/listeners.js
@@ -1,7 +1,9 @@
 import deleteForm from "./data.js";
 import getInput from "../../utils/getInput.util.js";
 import onFormSubmit from "../../utils/onFormSubmit.util.js";
-import validateInput from "../../utils/validateInput.util.js"
+import validateInput from "../../utils/validateInput.util.js";
+import BST from "../../data/tree.data.js";
+import {successModal, errorModal} from "../modals/data.js"
 
 export default function initListeners() {
   getInput("delete-item-input", (value) => {
@@ -15,7 +17,19 @@ export default function initListeners() {
       deleteForm.error.set("cannot delete empty item");
       return;
     }
-    //TODO delete item from tree
-    console.log(deleteForm);
+    
+    const deleted = BST.delete(deleteForm.value.get());
+    if(deleted) {
+      successModal.message = "deleted"
+      successModal.open.set(true);
+    } else {
+      errorModal.message = "element not found"
+      errorModal.open.set(true);
+    }
+
+    // reset values
+    const inputElement = window.document.getElementById("delete-item-input");
+    inputElement.value = "";
+    deleteForm.value.set("");
   });
 }
