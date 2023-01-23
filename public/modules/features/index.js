69326c2a0ca927f89cb550850f68427f09048d51
Daniel Ugbeye <ugbeyellionz@gmail.com>
2023-01-23 18:30:30 +0100

    task: exposed the tree traversal feature

--- public/modules/features/index.js
+++ public/modules/features/index.js
@@ -2,10 +2,12 @@ import initializeAddForm from "./add/index.js";
 import initializeDeleteForm from "./delete/index.js";
 import initializeModals from "./modals/index.js";
 import initializeSearchForm from "./search/index.js";
+import initializeTreeTraversals from "./tree-traversal/index.js";
 
 export default function initializefeatures() {
   initializeAddForm();
   initializeDeleteForm();
   initializeSearchForm();
   initializeModals();
+  initializeTreeTraversals();
 }
