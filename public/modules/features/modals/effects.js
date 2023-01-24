0d1e0ecb37cd2bcf742d28b7ebed6d8813af6598
Daniel Ugbeye <ugbeyellionz@gmail.com>
2023-01-24 05:16:14 +0100

    feat: added new method field to tree modal data and implemented animated effects for modals

--- public/modules/features/modals/effects.js
+++ public/modules/features/modals/effects.js
@@ -4,36 +4,44 @@ export default function initEffects() {
   successModal.open.subscribe((state) => {
     const backdropElement = window.document.getElementById("modal-backdrop");
     const successModalElement = window.document.getElementById("success-modal");
-    const successMessageElement = window.document.getElementById("success-modal-message");
+    const successMessageElement = window.document.getElementById(
+      "success-modal-message"
+    );
     if (state) {
       successMessageElement.innerHTML = successModal.message;
-      backdropElement.classList.remove("hidden");
-      backdropElement.classList.add("grid");
+      backdropElement.classList.remove("invisible", "pointer-events-none", "opacity-0");
+      backdropElement.classList.add("grid", "opacity-100");
 
-      successModalElement.classList.remove("hidden");
-    } else {
-      backdropElement.classList.remove("grid");
-      backdropElement.classList.add("hidden");
+      successModalElement.classList.remove("scale-0");
+      successModalElement.classList.add("-translate-y-[50%]", "scale-100");
+   } else {
+      backdropElement.classList.remove("grid", "opacity-100");
+      backdropElement.classList.add("invisible", "pointer-events-none", "opacity-0");
 
-      successModalElement.classList.add("hidden");
+      successModalElement.classList.remove("-translate-y-[50%]", "scale-100");
+      successModalElement.classList.add("scale-0");
     }
   });
 
   errorModal.open.subscribe((state) => {
     const backdropElement = window.document.getElementById("modal-backdrop");
     const errorModalElement = window.document.getElementById("error-modal");
-    const errorMessageElement = window.document.getElementById("error-modal-message");
+    const errorMessageElement = window.document.getElementById(
+      "error-modal-message"
+    );
     if (state) {
       errorMessageElement.innerHTML = errorModal.message;
-      backdropElement.classList.remove("hidden");
-      backdropElement.classList.add("grid");
+      backdropElement.classList.remove("invisible", "pointer-events-none", "opacity-0");
+      backdropElement.classList.add("grid", "opacity-100");
 
-      errorModalElement.classList.remove("hidden");
+      errorModalElement.classList.remove("scale-0");
+      errorModalElement.classList.add("-translate-y-[50%]", "scale-100");
     } else {
-      backdropElement.classList.remove("grid");
-      backdropElement.classList.add("hidden");
+      backdropElement.classList.remove("grid", "opacity-100");
+      backdropElement.classList.add("invisible", "pointer-events-none", "opacity-0");
 
-      errorModalElement.classList.add("hidden");
+      errorModalElement.classList.remove("-translate-y-[50%]", "scale-100");
+      errorModalElement.classList.add("scale-0");
     }
   });
 
@@ -41,17 +49,21 @@ export default function initEffects() {
     const backdropElement = window.document.getElementById("modal-backdrop");
     const treeModalElement = window.document.getElementById("tree-modal");
     const treeListElement = window.document.getElementById("tree-list");
+    const treeMethodElement = window.document.getElementById("tree-method");
     if (state) {
+      treeMethodElement.innerHTML = treeModal.method.get();
       treeListElement.innerHTML = treeModal.data.join(", ");
-      backdropElement.classList.remove("hidden");
-      backdropElement.classList.add("grid");
+      backdropElement.classList.remove("invisible", "pointer-events-none", "opacity-0");
+      backdropElement.classList.add("grid", "opacity-100");
 
-      treeModalElement.classList.remove("hidden");
+      treeModalElement.classList.remove("scale-0");
+      treeModalElement.classList.add("-translate-y-[50%]", "scale-100");
     } else {
-      backdropElement.classList.remove("grid");
-      backdropElement.classList.add("hidden");
+      backdropElement.classList.remove("grid", "opacity-100");
+      backdropElement.classList.add("invisible", "pointer-events-none", "opacity-0");
 
-      treeModalElement.classList.add("hidden");
+      treeModalElement.classList.remove("-translate-y-[50%]", "scale-100");
+      treeModalElement.classList.add("scale-0");
     }
   });
 }
