62ffabbb94a61fe84a957337a1a1b529f42efd68
Daniel Ugbeye <ugbeyellionz@gmail.com>
2023-01-25 12:05:31 +0100

    fix: worked on BST find algorithm to include empty tree

--- public/modules/binary-search-tree/index.js
+++ public/modules/binary-search-tree/index.js
@@ -50,7 +50,7 @@ class BinaryTree {
 
   find(data) {
     let current = this.root;
-    while (current.data !== data) {
+    while (current && current.data !== data) {
       if (data < current.data) {
         current = current.left;
       } else {
